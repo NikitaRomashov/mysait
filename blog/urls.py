1f4e0b40962a6b17e69789651023cf9cb3dea64d
Nikita Romashov <nikitos1654@gmail.com>
2021-02-13 12:59:54 +1000

    Иконка сайта

--- blog/urls.py
+++ blog/urls.py
@@ -1,19 +1,23 @@
 from django.urls import path, include
 from .import views
+from django.conf.urls import url
+from django.views.generic import RedirectView
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='home'), 
+    path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('history', views.history, name='history'),
     path('search/', views.searchInArticles, name='search_in_articles'),
     path('post/<int:article_id>', views.ArticleDetailView, name='article-detail'),
-    path('post/<int:article_id>/leave_comment', views.leave_comment, name='leave_comment'),
-    path('sponsor/<int:sponsor_id>', views.SponsorDetailView, name='sponsor-detail'),
+    path('post/<int:article_id>/leave_comment',
+         views.leave_comment, name='leave_comment'),
+    path('sponsor/<int:sponsor_id>',
+         views.SponsorDetailView, name='sponsor-detail'),
+    url(r'^favicon\.ico$', RedirectView.as_view(
+        url='/static/blog/img/favicon.ico', permanent=True)),
     #path('email', include('send_email.urls')),
-	 #для поиска 
-    
+    # для поиска
+
 
 ]
-
-
