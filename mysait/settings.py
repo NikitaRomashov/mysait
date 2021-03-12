74b9dd51d2ea8a4e53b31ae79b0aed02f782c32e
Nikita Romashov <nikitos1654@gmail.com>
2021-03-12 11:17:31 +1000

    new

--- mysait/settings.py
+++ mysait/settings.py
@@ -101,14 +101,13 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'romashov$',
+        'NAME': 'romashov$romashov_bd',
         'USER': 'romashov',
         'PASSWORD': '98201544Q',
         'HOST': 'romashov.mysql.pythonanywhere-services.com',
     }
 
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
